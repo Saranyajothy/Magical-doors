1f548d8c83955a2cdd4ec7f4fa9debaa41cff198
Saranyajothy <saranyajothikumar12@gmail.com>
2022-05-04 16:09:38 +0000

    deleted the code

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 Magical door game
 """
 import time
-import json
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -11,7 +10,6 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-creds = json.load(open('creds.json'))
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
